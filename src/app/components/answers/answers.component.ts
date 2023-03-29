94fa869b3fc7575f415f03dbd35684c2113a490d
Arturro <arturro_1996@o2.pl>
2023-03-29 20:26:00 +0200

    add answers component implementation

--- src/app/components/answers/answers.component.ts
+++ src/app/components/answers/answers.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { WordType } from 'src/app/data/model';
+import { WordsService } from 'src/app/services/words.service';
 
 @Component({
   selector: 'app-answers',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AnswersComponent {
 
+  @Input() title: string | undefined;
+  @Input() words: WordType[] | undefined;
+
+  constructor(private wordService: WordsService){}
 }
