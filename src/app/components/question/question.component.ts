f8f27a77b6c7b295754887edafc172ede94c2386
Arturro <arturro_1996@o2.pl>
2023-03-29 20:25:20 +0200

    add question implementation

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WordType } from 'src/app/data/model';
+import { WordsService } from 'src/app/services/words.service';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
-export class QuestionComponent {
+export class QuestionComponent implements OnInit {
 
+  word: WordType | undefined;
+
+  constructor(private wordService: WordsService){}
+
+  ngOnInit(): void {
+    this.fetchWord();
+  }
+
+  addToNouns(word: WordType){
+    this.wordService.addNouns(word);
+    this.fetchWord();
+  }
+  addToVerbs(word: WordType){
+    this.wordService.addVerb(word);
+    this.fetchWord();
+  }
+  check(): void{
+    this.wordService.check();
+  }
+
+  private fetchWord(): void {
+    this.word = this.wordService.getWords().shift();
+  }
 }
